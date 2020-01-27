145babe5ac54045382eee46a146c897d95ef3742
yoshinobu.inoue <yoshinobu.inoue@emtg.co.jp>
2020-01-27 20:54:47 +0900

    test

--- sandbox/java/src/factorial/FactorialTest.java
+++ sandbox/java/src/factorial/FactorialTest.java
@@ -9,9 +9,10 @@ public class FactorialTest {
 		try {
 			inChar = System.in.read();
 			System.out.println("You entered ");
-			System.out.println("updated ");
-			System.out.println("updated2 ");
-			System.out.println("updated3 ");
+			System.out.println("updated5 ");
+			System.out.println("updated5 ");
+			System.out.println("updated5 ");
+			System.out.println("updated5 ");
 			System.out.println(inChar);
 		} catch (IOException ioe) {
 			
